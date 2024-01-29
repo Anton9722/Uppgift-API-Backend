87ef5de4ec6fa68d606b6008728bdd048c938517
Anton9722 <107687551+Anton9722@users.noreply.github.com>
2024-01-29 10:31:14 +0100

    Add files via upload

---
+++ src/main/java/com/UppgiftApiBackend/UppgiftApiBackend/UppgiftApiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.UppgiftApiBackend.UppgiftApiBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UppgiftApiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UppgiftApiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UppgiftApiBackend/UppgiftApiBackend/UppgiftApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UppgiftApiBackend.UppgiftApiBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UppgiftApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
